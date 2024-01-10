5169255414c936a99b40187c7a3931952bbc494f
uchumik <uchiumi.k@gmail.com>
2024-01-10 14:41:06 +0900

    updated

---
+++ tg/include/hlattice.h
@@ -0,0 +1,24 @@
+#ifndef NPBNLP_HLATTICE_H
+#define NPBNLP_HLATTICE_H
+
+#include"io.h"
+#include<vector>
+namespace npbnlp {
+	class hlattice {
+		public:
+			hlattice(io& f, int i);
+			virtual ~hlattice();
+			word& wd(int i);
+			word& wp(int i);
+			int size(int i);
+			void slice(int i, double u);
+			double u(int i);
+			std::vector<int>::iterator begin(int i);
+			std::vector<int>::iterator end(int i);
+			std::vector<double> mu;
+			std::vector<std::vector<int> > k;
+		private:
+	};
+}
+
+#endif
---
+++ tg/include/ihmm.h
@@ -0,0 +1,45 @@
+#ifndef NPBNLP_IHMM
+#define NPBNLP_IHMM
+
+#include"io.h"
+#include"hpyp.h"
+#include"vpyp.h"
+#include"vtable.h"
+#include"hlattice.h"
+#include<functional>
+#include<memory>
+
+namespace npbnlp {
+	class ihmm {
+		public:
+			ihmm();
+			ihmm(int n, int k);
+			virtual ~ihmm();
+			virtual sentence sample(io& f, int i);
+			virtual sentence parse(io& f, int i);
+			virtual void add(sentence& s);
+			virtual void remove(sentence& s);
+			virtual void estimate(int iter);
+			virtual void poisson_correction(int n = 3000);
+			virtual void set(int v, int k);
+			virtual int n();
+			virtual int k();
+			virtual void slice(double a, double b);
+			virtual void save(const char *file);
+			virtual void load(const char *file);
+		protected:
+			int _n;
+			int _v;
+			int _k;
+			int _K;
+			double _a;
+			double _b;
+			std::shared_ptr<hpyp> _pos;
+			std::shared_ptr<std::vector<std::shared_ptr<hpyp> > > _word;
+			std::shared_ptr<std::vector<std::shared_ptr<vpyp> > > _letter;
+			std::mutex _mutex;
+		private:
+	};
+}
+
+#endif
