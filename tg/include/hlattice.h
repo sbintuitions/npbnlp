38a4c192ec71ba24798285dcd94d4acf01cca4ae
uchumik <uchiumi.k@gmail.com>
2024-01-13 16:15:22 +0900

    added lattice class for ihmm

--- tg/include/hlattice.h
+++ tg/include/hlattice.h
@@ -2,14 +2,16 @@
 #define NPBNLP_HLATTICE_H
 
 #include"io.h"
+#include"sentence.h"
 #include<vector>
+#include<memory>
 namespace npbnlp {
 	class hlattice {
 		public:
 			hlattice(io& f, int i);
 			virtual ~hlattice();
 			word& wd(int i);
-			word& wp(int i);
+			word* wp(int i);
 			int size(int i);
 			void slice(int i, double u);
 			double u(int i);
@@ -17,6 +19,7 @@ namespace npbnlp {
 			std::vector<int>::iterator end(int i);
 			std::vector<double> mu;
 			std::vector<std::vector<int> > k;
+			sentence s;
 		private:
 	};
 }
---
+++ tg/src/hlattice.cc
@@ -0,0 +1,62 @@
+#include"hlattice.h"
+#ifdef _OPENMP
+#include<omp.h>
+#endif
+
+using namespace std;
+using namespace npbnlp;
+
+static word eos;
+static vector<int> bos(1, 0);
+
+hlattice::hlattice(io& f, int i) {
+	int head = f.head[i];
+	int tail = f.head[i+1];
+	s = sentence(*f.raw, head, tail);
+	mu.resize(s.size(), 0);
+	k.resize(s.size());
+}
+
+hlattice::~hlattice() {
+}
+
+word& hlattice::wd(int i) {
+	if (i < 0 || i >= s.size())
+		return eos;
+	return s.wd(i);
+}
+
+word* hlattice::wp(int i) {
+	if (i < 0 || i >= s.size())
+		return &eos;
+	return &s.w[i];
+}
+
+int hlattice::size(int i) {
+	if (i < 0 || i >= k.size())
+		return 1;
+	return k[i].size();
+}
+
+void hlattice::slice(int i, double u) {
+	if (i >= 0 && i < s.size())
+		mu[i] = u;
+}
+
+double hlattice::u(int i) {
+	if (i < 0 || i >= s.size())
+		return 0;
+	return mu[i];
+}
+
+vector<int>::iterator hlattice::begin(int i) {
+	if (i < 0 || i >= s.size())
+		return bos.begin();
+	return k[i].begin();
+}
+
+vector<int>::iterator hlattice::end(int i) {
+	if (i < 0 || i >= s.size())
+		return bos.end();
+	return k[k].end();
+}
