b4790aeb18fb59e18e2abd724adeb4db71ddc6db
uchumik <uchiumi.k@gmail.com>
2025-02-21 23:02:04 +0900

    added hpy_sample.cc

---
+++ lm/src/hpy_sample.cc
@@ -0,0 +1,40 @@
+#include"hpy.h"
+#include"io.h"
+#include"util.h"
+#include"rd.h"
+#include<cmath>
+
+using namespace std;
+using namespace npbnlp;
+
+double perplexity(vector<sentence>& c, hpy& lm) {
+	double ll = 0;
+	double v = 0;
+	for (auto& s : c) {
+		ll += lm.lp(s);
+		v += s.size();
+	}
+	return exp(-ll/v);
+}
+
+int main(int argc, char **argv) {
+	int n = 3;
+	hpy lm(n);
+	io f(*(argv+1));
+
+	vector<sentence>data;
+	util::store_sentences(f, data);
+	for (auto i = 0; i < 100; ++i) {
+		int rd[data.size()] = {0};
+		rd::shuffle(rd, data.size());
+		for (auto j = 0; j < (int)data.size(); ++j) {
+			if (i > 0)
+				lm.remove(data[rd[j]]);
+			lm.add(data[rd[j]]);
+		}
+		lm.estimate(20);
+		cout << i << " train ppl:" << perplexity(data, lm) << endl;
+	}
+	lm.save("hpy.model");
+	return 0;
+}
